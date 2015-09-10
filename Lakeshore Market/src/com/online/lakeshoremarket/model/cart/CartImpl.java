b3a781a34aba9ec0960ee74e7bd7e7da0adca595
Rayyan <rlnu@luc.edu>
2015-09-10 07:44:34 -0500

    Commit 5

--- Lakeshore Market/src/com/online/lakeshoremarket/model/cart/CartImpl.java
+++ Lakeshore Market/src/com/online/lakeshoremarket/model/cart/CartImpl.java
@@ -5,4 +5,5 @@ public class CartImpl {
 	private String test;
 	private String test2;
 	private String test3;
+	private String test4;
 }
---
+++ Lakeshore Market/src/com/online/lakeshoremarket/model/product/ProductImpl.java
@@ -0,0 +1,5 @@
+package com.online.lakeshoremarket.model.product;
+
+public class ProductImpl {
+
+}
